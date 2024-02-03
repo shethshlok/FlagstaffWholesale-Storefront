fbfe118866559a78ed2101822f3f727f938ad64c
Shlok Sheth <ss3756@nau.edu>
2024-02-03 22:45:27 +0000

    thanking god I fixed the next config js domain problem for the images

--- next.config.js
+++ next.config.js
@@ -8,24 +8,25 @@ const nextConfig = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-      },
-    ],
+    domains: ["72.167.151.168"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "http",
+    //     hostname: "72.167.151.168:9000",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "medusa-server-testing.s3.amazonaws.com",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+    //   },
+    // ],
   },
 })
 
