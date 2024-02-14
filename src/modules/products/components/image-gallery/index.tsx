4ee077c676e6f1b430034a6ae31acb83f50a2ba5
shethshlok <ss3756@nau.edu>
2024-02-14 23:32:22 +0000

    added image carousal

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -1,20 +1,33 @@
+'use client';
 import { Image as MedusaImage } from "@medusajs/medusa"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 type ImageGalleryProps = {
   images: MedusaImage[]
 }
 
 const ImageGallery = ({ images }: ImageGalleryProps) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  }
   return (
-    <div className="flex items-start relative">
-      <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
+      // <div className="flex items-start relative">
+        <div className="items-start small:mx-16 gap-y-4 relative">
+      <Slider {...settings}>
         {images.map((image, index) => {
           return (
             <Container
               key={image.id}
-              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
+                className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle border border-gray-300"
               id={image.id}
             >
               <Image
@@ -31,8 +44,9 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
             </Container>
           )
         })}
-      </div>
-    </div>
+        </Slider>
+       </div>
+     // </div>
   )
 }
 
--- src/modules/products/templates/index.tsx
+++ src/modules/products/templates/index.tsx
@@ -30,11 +30,11 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
   return (
     <>
       <div className="content-container flex flex-col small:flex-row small:items-start py-6 relative">
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
+        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[250px] w-full py-8 gap-y-6">
           <ProductInfo product={product} />
           <ProductTabs product={product} />
         </div>
-        <div className="block w-full relative">
+        <div className="block w-1/2 relative">
           <ImageGallery images={product?.images || []} />
         </div>
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[400px] w-full py-8 gap-y-12">
